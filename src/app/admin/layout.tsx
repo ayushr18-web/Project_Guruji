439841d3161b23e1301811210da01fbfd11d5b6a
ayushr18-web <ayushmrv@gmail.com>
2025-06-09 00:28:12 +0530

    update tsconfig

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,7 +3,6 @@
 import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AdminSidebar  from '../../../components/AdminSidebar';
-import Header from '../../../components/Header';
 
 const queryClient = new QueryClient();
 
